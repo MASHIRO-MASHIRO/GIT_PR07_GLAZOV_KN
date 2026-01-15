c30245240f60399650cae2ba1e4830f7779d2507
Glazov <glazov@mail.ru>
2026-01-15 21:37:34 +0300

    Переход на форму администратора

--- PR07/Form2.Designer.cs
+++ PR07/Form2.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -38,11 +39,22 @@
             this.textBox1.Size = new System.Drawing.Size(155, 26);
             this.textBox1.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Переход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form2";
             this.Text = "Администратор";
@@ -54,5 +66,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- PR07/Form2.cs
+++ PR07/Form2.cs
@@ -16,5 +16,10 @@ namespace PR07
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
